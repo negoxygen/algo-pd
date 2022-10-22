f04b40f00929e0c9a4cb79822ebdb3c5942735e0
Aditi Acharya <aditi@gurzu.com>
2022-10-22 12:22:44 +0545

    fix:solution for the correct sum

--- Algo/2.EvenFibonacciNumbers/solution1.js
+++ Algo/2.EvenFibonacciNumbers/solution1.js
@@ -3,14 +3,12 @@ const fiboEvenSum = (number) => {
     return 0;
   } else {
     let evenSum = 0,
-      prevFibNum = 1,
       // According to problem description our Fibonacci series starts with 1, 2
       fibNum = 2; 
-    for (let i = 2; fibNum <= number; i++) {
-      if (fibNum % 2 == 0) {
+    for (let i = 2; fibNum <= number; fibNum++) {
+      if (fibNum % 2 === 0) {
         evenSum += fibNum;
       }
-      [prevFibNum, fibNum] = [fibNum, prevFibNum + fibNum];
     }
     return evenSum;
   }
