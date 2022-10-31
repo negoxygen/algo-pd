20b34d06c3b3ba5b7d91f023804d8da82eda4c15
rivab <bhandarireeva@gmail.com>
2022-10-31 13:32:37 +0545

    Filter key/value using select

--- Algo/8.FilterHashKey/filter_hash.rb
+++ Algo/8.FilterHashKey/filter_hash.rb
@@ -1,13 +1,26 @@
 class FilterHash
+
+    # Using slice
     def key_slice(hash_value,filter_key)
-      # Using slice
-      selected_value = hash_value.slice(:"#{filter_key}")
-      puts selected_value
+      hash_value.slice(:"#{filter_key}")
+    end
+
+    # Using select
+    def key_filter(hash_value,filter_key)
+      hash_value.select {|key| key.to_s.match(filter_key) }
     end
   
 end
 
-# hash_value = { "a": "October" , b: "November", c: "December" }
-# FilterHash.new.key_slice(hash_value,"a")
+# hash_value = { "a": "October" , "b": "November", c: "December" }
+
+# selected_value = FilterHash.new.key_slice(hash_value,"a")
+# puts selected_value
 # Output:
 # {:a=>"October"}
+
+# selected_value = FilterHash.new.key_filter(hash_value,"b")
+# puts selected_value
+# Output:
+#{:b=>"November"}
+
